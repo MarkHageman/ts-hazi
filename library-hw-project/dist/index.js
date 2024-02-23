75ca67f339631cb297d31c953bbd85d47d8ab0a8
MarkHageman <suhagi94@gmail.com>
2024-02-23 21:14:23 +0100

    npm projekt létrehozása, függőségek telepítése, konfig fájlok beállítása, npm start tesztelése

---
+++ library-hw-project/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const hello = function (name) {
+    return `Hello, ${name}!`;
+};
+console.log(hello("Márk"));
---
+++ library-hw-project/src/index.ts
@@ -0,0 +1,5 @@
+const hello = function(name: string): string {
+    return `Hello, ${name}!`
+}
+
+console.log(hello("Márk"));
