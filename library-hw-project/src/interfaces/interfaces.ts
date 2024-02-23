309bfc7b7a82d37b9d2bfff30132b403ae3e07f3
MarkHageman <suhagi94@gmail.com>
2024-02-23 22:59:29 +0100

    interfacek kialakítása

--- library-hw-project/src/interfaces/interfaces.ts
+++ library-hw-project/src/interfaces/interfaces.ts
@@ -1,3 +1,41 @@
-interface Name {
-    name: string
+interface Book {
+    ISBN: number,
+    author: string,
+    title: string,
+    publisher: string,
+    publicationDate: Date,
+    pageCount: number,
+    inStock: boolean
+
+}
+
+interface LibraryStock {
+    books: Book[],
+    //crud
+    addNewBook(book: Book): void,
+    listAllBooks(): Book[],
+    searchOneBook<T extends number | string>(identifier: T): Book | undefined,
+    updateBook<T extends number | string>(identifier: T, updatedBook: Book): void,
+    deleteFromStock(id: number): string
+}
+
+interface BorrowerContacts {
+    phoneNumber: string,
+    email: string,
+
+}
+
+interface Borrower {
+    name: string,
+    borrowedBooks: BorrowedBook[],
+    borrowerContacts: BorrowerContacts
+
+}
+
+interface BorrowedBook {
+    bookISBN: Book["ISBN"],
+    bookTitle: Book["title"],
+    borrowingStartDate: Date,
+    borrowingExpireDate: Date
+
 }
